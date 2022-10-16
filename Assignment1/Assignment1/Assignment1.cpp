056d03c65e740d75e075031c982da9c34661c687
Adi Peterca <adipeterca@gmail.com>
2022-10-16 17:04:36 +0300

    Refactored code & added test functions for better understanding of the overall development process

--- Assignment1/Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1/Assignment1.cpp
@@ -11,6 +11,67 @@
 extern double* OShift, * M, * y, * z, * x_bound;
 extern int ini_flag, n_flag, func_flag, * SS;
 
+/// <summary>
+/// Quick testing function to understand the input data for Rastrigin function.
+/// </summary>
+void testValue()
+{
+	const int dims = 10;
+	double x[dims];
+
+	std::ifstream fin("input_data/shift_data_4.txt");
+	for (int i = 0; i < dims; i++)
+		fin >> x[i];
+
+	double returnValue;
+	cec19_test_func(x, &returnValue, dims, 1, 4);
+
+	std::cout << returnValue << std::endl;
+}
+
+/// <summary>
+/// Runs the initial given sample code for the specified function.
+/// </summary>
+/// <param name="currentFunction">the specified function. Values between 1-10, defaults to 4 (Rastrigin).</param>
+void testFunction(int currentFunction =4)
+{
+	const auto n = 10;
+	const auto m = 2;
+
+	double* x = new double[m * n * sizeof(double)];
+	double* f = new double[m * sizeof(double)];
+
+	std::ifstream fin("input_data/shift_data_" + std::to_string(currentFunction) + ".txt");
+	if (!fin)
+	{
+		std::cout << "Error: Cannot open input file for reading: input_data/shift_data_" + std::to_string(currentFunction) + ".txt\n";
+		return;
+	}
+
+	std::cout << "Lowest point: ";
+	for (int k = 0; k < n; k++)
+	{
+		fin >> x[k];
+		std::cout << x[k] << " ";
+	}
+	std::cout << std::endl;
+
+	fin.close();
+
+	for (int j = 0; j < n; j++)
+	{
+		// why is it 1*n?
+		// LE: we skip pass the first N numbers because we already read them ^
+		//x[1 * n + j] = 0.0;
+		//printf("%Lf\n",x[1*n+j]);
+
+		x[n + j] = 0.0;
+	}
+
+	cec19_test_func(x, f, n, m, currentFunction);
+	std::cout << "Result for lowest point: " << f[0] << std::endl;
+	std::cout << "Result for all zero point: " << f[1] << std::endl;
+}
 
 int main()
 {
@@ -18,62 +79,8 @@ int main()
 	std::cout << std::fixed << std::showpoint;
 	std::cout << std::setprecision(5);
 
-	// Ranges from 1 to 10
-	int currentFunction;
-
-	const auto n = 10;
-	const auto m = 2;
-
-	double* x = new double[m * n * sizeof(double)];
-	double* f = new double[m * sizeof(double)];
-
-	std::ifstream fin;
-	std::string filename;
-
-	for (int i = 3; i < 4; i++)
-	{
-		currentFunction = i + 1;
-		filename = "input_data/shift_data_" + std::to_string(currentFunction) + ".txt";
-		fin.open(filename);
-		if (!fin)
-		{
-			std::cout << "Error: Cannot open input file for reading: " + filename + "\n";
-			return -1;
-		}
-
-		// x - vector for shift_data
-		for (int k = 0; k < n; k++)
-		{
-			fin >> x[k];
-			std::cout << "Read for shift_data: " << x[k] << std::endl;
-		}
-
-		fin.close();
-
-		for (int j = 0; j < n; j++)
-		{
-			// why is it 1*n?
-			// LE: we skip pass the first N numbers because we already read them ^
-			//x[1 * n + j] = 0.0;
-			//printf("%Lf\n",x[1*n+j]);
-
-			x[n + j] = 0.0;
-			std::cout << "second for: " << x[n + j] << std::endl;
-		}
-
-		// Why k=0,1?
-		for (int k = 0; k < 1; k++)
-		{
-
-			cec19_test_func(x, f, n, m, currentFunction);
-			for (int j = 0; j < 2; j++)
-			{
-				printf(" f%d(x[%d]) = %Lf,", currentFunction, j + 1, f[j]);
-			}
-			std::cout << std::endl;
-		}
-
-	}
+	testValue();
+	testFunction();
 
 	return 0;
 }
