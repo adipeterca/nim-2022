cf0e333cdb50b203d7a4b81bea7f0c9962d5eb32
NicolaeCarpatina <nicucapatina2000@gmail.com>
2022-12-13 11:01:58 +0200

    Added testing env

--- Assignment1/Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1/Assignment1.cpp
@@ -10,6 +10,9 @@
 #include "jDE100v2.h"
 #include "PSO.h"
 #pragma warning(disable:4996)
+#define NO_EVALS 10000000
+#define NO_THREADS 16
+#define NO_ITERATIONS 10
 
 using namespace std;
 
@@ -58,26 +61,26 @@ public:
 	}
 };
 
-void test(Function* f1, AbstractFunction* f2) {
-	
-	size_t noEvals = 1e5;
+void test(Function* f1, AbstractFunction* f2, ofstream & fout) {
+
+	size_t noEvals = NO_EVALS;
 	vector<double> point;
 	point.resize(f1->getDimensions());
 
+	fout << "Starting evaluating using OLD method\n";
 	auto t1 = chrono::high_resolution_clock::now();
-	cout << "Starting evaluating " << noEvals << " evaluations using OLD method\n";
 	for (int i = 0; i < noEvals; i++) {
 		for (int j = 0; j < point.size(); j++) {
 			point[j] = getRandomDouble(f1->getMin(), f1->getMax());
 		}
 		(*f1)(point);
 	}
-	auto nanoseconds = chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now() - t1).count();
-	cout << "Evaluation took " << nanoseconds << " nanoseconds\n";
+	auto milliseconds = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - t1).count();
+	fout << "Evaluation took " << milliseconds << " milliseconds\n";
 
-	size_t noThreads = 2;
+	size_t noThreads = NO_THREADS;
 	vector<thread> threads;
-	cout << "Starting evaluating " << noEvals << " evaluations using NEW method (using " << noThreads << " threads)\n";
+	fout << "Starting evaluating using NEW method\n";
 	t1 = chrono::high_resolution_clock::now();
 
 	auto func = [&f1, &f2](int iters) {
@@ -99,25 +102,63 @@ void test(Function* f1, AbstractFunction* f2) {
 	for (int i = 0; i < noThreads; i++) {
 		threads[i].join();
 	}
-	nanoseconds = chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now() - t1).count();
-	cout << "Evaluation took " << nanoseconds << " nanoseconds\n";
+	milliseconds = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - t1).count();
+	fout << "Evaluation took " << milliseconds << " milliseconds\n";
+}
+
+void test2(Function* f1, AbstractFunction* f2, long double& _old, long double& _new) {
+	// How many function evaluations to take into account
+	size_t noEvals = NO_EVALS;
+	vector<vector<double>> points;
+	points.resize(noEvals);
+	for (auto& point : points) {
+		for (int j = 0; j < f1->getDimensions(); j++) {
+			point.push_back(getRandomDouble(f1->getMin(), f1->getMax()));
+		}
+	}
+	size_t noThreads = NO_THREADS;
+	vector<thread> threads;
+
+	auto t1 = chrono::high_resolution_clock::now();
+	for (auto& point : points) {
+		(*f1)(point);
+	}
+	_old = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - t1).count();
+
+	t1 = chrono::high_resolution_clock::now();
+	auto func = [&f1, &f2, &points](int start, int len) {
+		for (int i = start; i < start + len; i++) {
+			(*f2)(points[i]);
+		}
+	};
+
+	// How many evaluations should each thread take
+	int evals = noEvals / noThreads;
+
+	for (int i = 0; i < noThreads; i++) {
+		threads.push_back(thread(func, i * evals, evals));
+	}
+	for (int i = 0; i < noThreads; i++) {
+		threads[i].join();
+	}
+	_new = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - t1).count();
 }
 
 int main()
 {
 	// Settings for cout
-	cout << fixed << showpoint << setprecision(10);
+	//cout << fixed << showpoint << setprecision(10);
 
 	vector<Function> functions; // difficulty ranking
 	functions.push_back(Function("Ackely", 10, 10, -100, 100)); // 1 done
-	//functions.push_back(Function("Chebyshev", 1, 9, -8192, 8192)); // 2 done
-	//functions.push_back(Function("Weierstrass", 6, 10, -100, 100)); // 3 done
-	//functions.push_back(Function("Griewank", 5, 10, -100, 100)); // 4 done
-	//functions.push_back(Function("Rastrigin", 4, 10, -100, 100)); // 5 done
-	//functions.push_back(Function("Lennard Jones", 3, 18, -4, 4)); // 6 done
-	//functions.push_back(Function("Hilbert", 2, 16, -16384, 16384)); // 7 done
+	functions.push_back(Function("Chebyshev", 1, 9, -8192, 8192)); // 2 done
+	functions.push_back(Function("Weierstrass", 6, 10, -100, 100)); // 3 done
+	functions.push_back(Function("Griewank", 5, 10, -100, 100)); // 4 done
+	functions.push_back(Function("Rastrigin", 4, 10, -100, 100)); // 5 done
+	functions.push_back(Function("Lennard Jones", 3, 18, -4, 4)); // 6 done
+	functions.push_back(Function("Hilbert", 2, 16, -16384, 16384)); // 7 done
 	functions.push_back(Function("Schwefel", 7, 10, -100, 100));// 8 done
-	//functions.push_back(Function("Escaffer 6", 8, 10, -100, 100)); // 9 done
+	functions.push_back(Function("Escaffer 6", 8, 10, -100, 100)); // 9 done
 	functions.push_back(Function("Happy Cat", 9, 10, -100, 100)); // 10 done
 
 	class Ackley ackley(10, 10, 1.0);
@@ -131,13 +172,61 @@ int main()
 	class Escaffer6 escaffer6(8, 10, 1.0);
 	class HappyCat happyCat(9, 10, 5.0 / 100.0);
 
-	//Test t(&ackley, 10, -100, 100);
-	//t.run(16);
-
-	for (int i = 0; i < 10; i++) {
-		//test(&functions[0], &ackley);
-		test(&functions[1], &schwefel);
-		cout << endl;
+	long double old_sum = 0, new_sum = 0, _old = 0, _new = 0;
+	ofstream fout("results_" + to_string(NO_THREADS) + ".txt");
+	fout << fixed << showpoint << setprecision(10);
+	AbstractFunction* f = nullptr;
+	fout << "Number of evaluations: " << NO_EVALS << "\n" << "Number of threads: " << NO_THREADS << "\n" << "Number of iterations: " << NO_ITERATIONS << "\n\n";
+	for (int j = 0; j < 1; ++j) {
+		if (j == 0) {
+			f = &ackley;
+			fout << "Testing function Ackley" << '\n';
+		}
+		else if (j == 1) {
+			f = &chebyshev;
+			fout << "Testing function Chebyshev" << '\n';
+		}
+		else if (j == 2) {
+			f = &weierstrass;
+			fout << "Testing function Weierstrass" << '\n';
+		}
+		else if (j == 3) {
+			f = &griewank;
+			fout << "Testing function Griewank" << '\n';
+		}
+		else if (j == 4) {
+			f = &rastrigin;
+			fout << "Testing function Rastrigin" << '\n';
+		}
+		else if (j == 5) {
+			f = &lennardJones;
+			fout << "Testing function Lennard Jones" << '\n';
+		}
+		else if (j == 6) {
+			f = &hilbert;
+			fout << "Testing function Hilbert" << '\n';
+		}
+		else if (j == 7) {
+			f = &schwefel;
+			fout << "Testing function Schwefel" << '\n';
+		}
+		else if (j == 8) {
+			f = &escaffer6;
+			fout << "Testing function Escaffer 6" << '\n';
+		}
+		else if (j == 9) {
+			f = &happyCat;
+			fout << "Testing function Happy Cat" << '\n';
+		}
+		old_sum = 0;
+		new_sum = 0;
+		for (int i = 0; i < 10; i++) {
+			test2(&functions[j], f, _old, _new);
+			old_sum += _old;
+			new_sum += _new;
+		}
+		fout << "Evaluation OLD took " << old_sum / NO_ITERATIONS << " milliseconds\n";
+		fout << "Evaluation NEW took " << new_sum / NO_ITERATIONS << " milliseconds\n";
 	}
 	return 0;
 }
